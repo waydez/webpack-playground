10bed0c474ee096a4370f68dc475460b04a83b67
waydez <acton8.wang@gmail.com>
2024-01-21 14:12:17 +0800

    增加 scss和 image 的 loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+import bg from './assets/bg.jpg'
+import style from './index.scss'
+
+// const img = new Image()
+// img.src = bg
+const styleElem = document.createElement('style')
+styleElem.append(style)
+
+const img = document.createElement('img')
+img.className = 'test'
+img.src = bg
+
+const root = document.getElementById('app')
+root.append(img)
+
 const title = '这里是 index.js！'
 
 console.log(`Hi!, ${title}`)
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,49 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
+  // entry: './src/index.js',
   entry: {
     main: './src/index.js',
     vendor: './src/vendor.js'
   },
+  // 默认输出到 dist/
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].[contenthash].bundle.js'
   },
-  plugins: [new HtmlWebpackPlugin()]
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          'style-loader',
+          // 将 CSS 转化成 CommonJS 模块
+          'css-loader',
+          // 将 Sass 编译成 CSS
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif)$/, // 针对这三种格式的文件使用file-loader处理
+        use: {
+          loader: 'url-loader',
+          options: {
+            // 定义打包后文件的名称；
+            // [name]:原文件名，[hash]:hash字符串（如果不定义名称，默认就以hash命名，[ext]:原文件的后缀名）
+            name: '[name]_[hash].[ext]',
+            outputPath: 'images/', //  定义图片输出的文件夹名（在output.path目录下）
+            limit: 204800 // 大于200kb的图片会被打包在images文件夹里面，小于这个值的会被以base64的格式写在js文件中
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html', // 通过chunk来指定引入哪些JS文件
+      chunk: ['index', 'vendor']
+    })
+  ]
 }
